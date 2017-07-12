0d474e270687de5b25b734fa4d44b125a7e7a57a
Mushtaq Ahmed <mushtaq.a@gmail.com>
2017-07-12 18:48:16 +0530

    use immutable actors in simple cases

--- csw-framework/src/main/scala/csw/vslice/assembly/TromboneControl.scala
+++ csw-framework/src/main/scala/csw/vslice/assembly/TromboneControl.scala
@@ -1,35 +1,24 @@
 package csw.vslice.assembly
 
 import akka.typed.scaladsl.Actor
-import akka.typed.scaladsl.Actor.MutableBehavior
 import akka.typed.{ActorRef, Behavior}
 import csw.vslice.framework.RunningHcdMsg.Submit
 import csw.vslice.hcd.models.TromboneHcdState
 
-class TromboneControl(ac: AssemblyContext, tromboneHCDIn: Option[ActorRef[Submit]])
-    extends MutableBehavior[TromboneControlMsg] {
-
-  var tromboneHCD: Option[ActorRef[Submit]] = tromboneHCDIn
-
-  override def onMessage(msg: TromboneControlMsg): Behavior[TromboneControlMsg] = {
-    msg match {
-      case GoToStagePosition(stagePosition) =>
-        assert(stagePosition.units == ac.stagePositionUnits)
-        val encoderPosition = Algorithms.stagePositionToEncoder(ac.controlConfig, stagePosition.head)
-        assert(
-          encoderPosition > ac.controlConfig.minEncoderLimit && encoderPosition < ac.controlConfig.maxEncoderLimit
-        )
-        tromboneHCD.foreach(_ ! Submit(TromboneHcdState.positionSC(ac.commandInfo, encoderPosition)))
-
-      case UpdateTromboneHCD(tromboneHCDIn1) =>
-        tromboneHCD = tromboneHCDIn1
-    }
-    this
-  }
-}
-
 object TromboneControl {
-  def behaviour(assemblyContext: AssemblyContext,
-                tromboneHCD: Option[ActorRef[Submit]] = None): Behavior[TromboneControlMsg] =
-    Actor.mutable(ctx ⇒ new TromboneControl(assemblyContext, tromboneHCD))
+  def behaviour(ac: AssemblyContext, tromboneHCD: Option[ActorRef[Submit]] = None): Behavior[TromboneControlMsg] =
+    Actor.immutable { (_, msg) ⇒
+      msg match {
+        case GoToStagePosition(stagePosition) =>
+          assert(stagePosition.units == ac.stagePositionUnits)
+          val encoderPosition = Algorithms.stagePositionToEncoder(ac.controlConfig, stagePosition.head)
+          assert(
+            encoderPosition > ac.controlConfig.minEncoderLimit && encoderPosition < ac.controlConfig.maxEncoderLimit
+          )
+          tromboneHCD.foreach(_ ! Submit(TromboneHcdState.positionSC(ac.commandInfo, encoderPosition)))
+          Actor.same
+        case UpdateTromboneHCD(tromboneHCDIn1) =>
+          behaviour(ac, tromboneHCDIn1)
+      }
+    }
 }
