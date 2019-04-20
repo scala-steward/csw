d9cb76e32b83f094b285ee9c325c7d1b5efb2bb1
Dollyg <dollyg@thoughtworks.com>
2019-04-20 17:35:50 +0530

    000: | cleanup

--- csw-event/csw-event-client/src/main/scala/csw/event/client/pb/TypeMapperSupport.scala
+++ csw-event/csw-event-client/src/main/scala/csw/event/client/pb/TypeMapperSupport.scala
@@ -1,175 +1,128 @@
-//package csw.event.client.pb
-//
-//import com.google.protobuf.ByteString
-//import csw.event.client.pb.TypeMapperFactory.make
-//import csw.params.core.generics.{KeyType, Parameter}
-//import csw.params.core.models.ObsId.empty
-//import csw.params.core.models._
-//import csw.params.events._
-//import csw.time.core.models.{TAITime, UTCTime}
-//import csw_protobuf.events.PbEvent
-//import csw_protobuf.events.PbEvent.PbEventType
-//import csw_protobuf.keytype.PbKeyType
-//import csw_protobuf.parameter._
-//import csw_protobuf.primitives._
-//import csw_protobuf.units.PbUnits
-//import play.api.libs.json.Format
-//import scalapb.TypeMapper
-//
-//import scala.collection.mutable
-//import scala.reflect.ClassTag
-//
-//object TypeMapperSupport {
-//
-//  implicit def parameterTypeMapper[P <: PbParamValue, S: ClassTag: Format](
-//      implicit typeMapper: TypeMapper[P, S]
-//  ): TypeMapper[PbParameter, Parameter[S]] =
-//    new TypeMapper[PbParameter, Parameter[S]] {
-//      override def toCustom(pbParameter: PbParameter): Parameter[S] = Parameter(
-//        pbParameter.name,
-//        pbParameter.keyType.asInstanceOf[KeyType[S]],
-//        cswItems(pbParameter.values),
-//        pbParameter.units
-//      )
-//
-//      override def toBase(x: Parameter[S]): PbParameter =
-//        PbParameter()
-//          .withName(x.keyName)
-//          .withUnits(x.units)
-//          .withKeyType(x.keyType)
-//          .withValues(x.items.map(d => encodeParamValue(d)))
-//    }
-//
-//  implicit val parameterTypeMapper2: TypeMapper[PbParameter, Parameter[_]] = {
-//    TypeMapper[PbParameter, Parameter[_]](
-//      p ⇒ make(p.keyType).toCustom(p)
-//    )(p => make(p.keyType).toBase(p))
-//  }
-//
-//  private def cswItems[T: ClassTag](items: Seq[PbParamValue]): mutable.WrappedArray[T] =
-//    items.map(x => decodeParamValue(x).asInstanceOf[T]).to[mutable.WrappedArray]
-//
-//  private val ParameterSetMapper =
-//    TypeMapper[Seq[PbParameter], Set[Parameter[_]]] {
-//      _.map(TypeMapperSupport.parameterTypeMapper2.toCustom).toSet
-//    } {
-//      _.map(TypeMapperSupport.parameterTypeMapper2.toBase).toSeq
-//    }
-//
-//  private[csw] implicit def eventTypeMapper[T <: Event]: TypeMapper[PbEvent, T] = new TypeMapper[PbEvent, T] {
-//    override def toCustom(base: PbEvent): T = {
-//      val factory: (Id, Prefix, EventName, UTCTime, Set[Parameter[_]]) ⇒ Any = base.eventType match {
-//        case PbEventType.SystemEvent     ⇒ SystemEvent.apply
-//        case PbEventType.ObserveEvent    ⇒ ObserveEvent.apply
-//        case PbEventType.Unrecognized(x) ⇒ throw new RuntimeException(s"unknown event type=[${base.eventType.toString} :$x]")
-//      }
-//
-//      factory(
-//        Id(base.eventId),
-//        Prefix(base.source),
-//        EventName(base.name),
-//        base.eventTime.map(utcTimeTypeMapper.toCustom).get,
-//        ParameterSetMapper.toCustom(base.paramSet)
-//      ).asInstanceOf[T]
-//    }
-//
-//    override def toBase(custom: T): PbEvent = {
-//      val pbEventType = custom match {
-//        case _: ObserveEvent ⇒ PbEventType.ObserveEvent
-//        case _: SystemEvent  ⇒ PbEventType.SystemEvent
-//      }
-//      PbEvent()
-//        .withEventId(custom.eventId.id)
-//        .withSource(custom.source.prefix)
-//        .withName(custom.eventName.name)
-//        .withEventTime(utcTimeTypeMapper.toBase(custom.eventTime))
-//        .withParamSet(ParameterSetMapper.toBase(custom.paramSet))
-//        .withEventType(pbEventType)
-//    }
-//  }
-//
-//  implicit val structTypeMapper: TypeMapper[PbStruct, Struct] = TypeMapper[PbStruct, Struct] { s =>
-//    Struct(s.paramSet.map(parameterTypeMapper2.toCustom).toSet)
-//  } { s =>
-//    PbStruct().withParamSet(s.paramSet.map(TypeMapperSupport.parameterTypeMapper2.toBase).toSeq)
-//  }
-//
-//  implicit val choiceTypeMapper: TypeMapper[PbChoice, Choice] = TypeMapper[PbChoice, Choice](
-//    p => Choice(p.value.name)
-//  )(x => PbChoice(x.name))
-//
-//  implicit val stringTypeMapper: TypeMapper[PbString, String] = TypeMapper[PbString, String](_.value)(x => PbString(x))
-//  implicit val intTypeMapper: TypeMapper[PbInt, Int]          = TypeMapper[PbInt, Int](_.value)(x => PbInt(x))
-//  implicit val shortTypeMapper: TypeMapper[PbShort, Short]    = TypeMapper[PbShort, Short](_.value.toShort)(x => PbShort(x))
-//  implicit val longTypeMapper: TypeMapper[PbLong, Long]       = TypeMapper[PbLong, Long](_.value)(x => PbLong(x))
-//  implicit val floatTypeMapper: TypeMapper[PbFloat, Float]    = TypeMapper[PbFloat, Float](_.value)(x => PbFloat(x))
-//  implicit val doubleTypeMapper: TypeMapper[PbDouble, Double] = TypeMapper[PbDouble, Double](_.value)(x => PbDouble(x))
-//  implicit val charTypeMapper: TypeMapper[PbChar, Char]       = TypeMapper[PbChar, Char](_.value.toChar)(x => PbChar(x))
-//  implicit val bytesTypeMapper: TypeMapper[PbBytes, Byte] =
-//    TypeMapper[PbBytes, Byte](_.value.toByteArray.head)(x => PbBytes(ByteString.copyFrom(scala.Array(x))))
-//  implicit val boolTypeMapper: TypeMapper[PbBoolean, Boolean] = TypeMapper[PbBoolean, Boolean](_.value)(x => PbBoolean(x))
-//
-//  implicit val unitsTypeMapper: TypeMapper[PbUnits, Units] =
-//    TypeMapper[PbUnits, Units](x ⇒ Units.withName(x.toString()))(x ⇒ PbUnits.fromName(x.toString).get)
-//
-//  implicit def matrixDataTypeMapper[T: ClassTag]: TypeMapper[PbMatrix, MatrixData[T]] =
-//    TypeMapper[PbMatrix, MatrixData[T]](
-//      x ⇒ MatrixData.fromArrays(x.values.toArray.map(a ⇒ a.values.map(decodePrimitive).toArray[T]))
-//    )(
-//      x ⇒ PbMatrix().withValues(x.data.map(a => PbArray().withValues(a.map(encodePrimitive))))
-//    )
-//
-//  implicit val raDecTypeMapper: TypeMapper[PbRaDec, RaDec] =
-//    TypeMapper[PbRaDec, RaDec] { case PbRaDec(Some(x)) ⇒ x }(x ⇒ PbRaDec().withValue(x))
-//
-//  implicit val utcTimeTypeMapper: TypeMapper[PbUTCTime, UTCTime] = TypeMapper[PbUTCTime, UTCTime](
-//    x ⇒ UTCTime(x.getValue)
-//  )(
-//    x ⇒ PbUTCTime().withValue(x.value)
-//  )
-//
-//  implicit val taiTimeTypeMapper: TypeMapper[PbTAITime, TAITime] = TypeMapper[PbTAITime, TAITime](
-//    x ⇒ TAITime(x.getValue)
-//  )(
-//    x ⇒ PbTAITime().withValue(x.value)
-//  )
-//
-//  implicit val keyTypeTypeMapper: TypeMapper[PbKeyType, KeyType[_]] =
-//    TypeMapper[PbKeyType, KeyType[_]](x ⇒ KeyType.withName(x.toString()))(x ⇒ PbKeyType.fromName(x.toString).get)
-//
-//  implicit def arrayDataTypeMapper[T: ClassTag]: TypeMapper[PbArray, ArrayData[T]] =
-//    TypeMapper[PbArray, ArrayData[T]](
-//      x ⇒ ArrayData(x.values.map(decodePrimitive).toArray[T])
-//    )(
-//      x ⇒ PbArray().withValues(x.data.map(encodePrimitive))
-//    )
-//
-//  implicit val obsIdTypeMapper: TypeMapper[String, Option[ObsId]] = TypeMapper[String, Option[ObsId]] { x ⇒
-//    if (x.isEmpty) None else Some(ObsId(x))
-//  } { x ⇒
-//    x.getOrElse(empty).obsId
-//  }
-//
-//  def encodeParamValue[P <: PbParamValue, T: ClassTag](paramValue: T)(implicit typeMapper: TypeMapper[P, T]): PbParamValue = {
-//    def encodeArray(xs: mutable.WrappedArray[T]) = PbArray(xs.map(x => encodePrimitive[P, T](x).asInstanceOf[PbPrimitive]))
-//
-//    paramValue match {
-//      case ArrayData(xs)   => PbParamValue(PbParamValue.Value.Array(encodeArray(xs)))
-//      case MatrixData(xss) => PbParamValue(PbParamValue.Value.Matrix(PbMatrix(xss.map(encodeArray))))
-//      case primitive: T    => PbParamValue().withPrimitive(encodePrimitive[P, T](primitive).asInstanceOf[PbPrimitive])
-//    }
-//  }
-//
-//  def encodePrimitive[P <: PbPrimitive, T](primitive: T)(implicit typeMapper: TypeMapper[P, T]): P = typeMapper.toBase(primitive)
-//
-//  def decodeParamValue[T](pbParamValue: PbParamValue): Any = pbParamValue.value match {
-//    case x: Primitive                   => decodePrimitive(x)
-//    case xs: PbParamValue.Value.Array   => xs.array.get.values.map(decodePrimitive)
-//    case xss: PbParamValue.Value.Matrix => xss.matrix.get.values.map(arr => arr.values.map(decodePrimitive))
-//  }
-//
-//  def decodePrimitive[T](primitive: Primitive): Any = primitive.value match {
-//    case x: Option[Any] => x.get
-//    case x              => x
-//  }
-//}
+package csw.event.client.pb
+
+import java.time.Instant
+
+import com.google.protobuf.ByteString
+import com.google.protobuf.timestamp.Timestamp
+import csw.params.core.generics.{KeyType, Parameter}
+import csw.params.core.models._
+import csw.params.events.{Event, EventName, ObserveEvent, SystemEvent}
+import csw.time.core.models.{TAITime, UTCTime}
+import csw_protobuf.events.PbEvent
+import csw_protobuf.events.PbEvent.PbEventType
+import csw_protobuf.keytype.PbKeyType
+import csw_protobuf.parameter._
+import csw_protobuf.units.PbUnits
+import play.api.libs.json.Format
+import scalapb.TypeMapper
+
+import scala.collection.mutable
+import scala.reflect.ClassTag
+
+object TypeMapperSupport {
+
+  lazy val utcTimeTypeMapper: TypeMapper[PbUTCTime, UTCTime] = TypeMapper[PbUTCTime, UTCTime](
+    x ⇒ UTCTime(Instant.ofEpochSecond(x.value.get.seconds, x.value.get.nanos))
+  )(
+    x ⇒ PbUTCTime(Some(Timestamp(x.value.getEpochSecond, x.value.getNano)))
+  )
+
+  lazy val paramValueMapper: TypeMapper[PbParamValue, Any] = TypeMapper[PbParamValue, Any] {
+    case PbInt(value)     => value
+    case PbLong(value)    => value
+    case PbFloat(value)   => value
+    case PbShort(value)   => value
+    case PbDouble(value)  => value
+    case PbChar(value)    => value
+    case PbBytes(value)   => value.toByteArray.head
+    case PbBoolean(value) => value
+    case PbString(value)  => value
+    case PbChoice(value)  => Choice(value)
+    case PbRaDec(ra, dec) => RaDec(ra, dec)
+    case PbTAITime(value) => TAITime(Instant.ofEpochSecond(value.get.seconds, value.get.nanos))
+    case value: PbUTCTime => utcTimeTypeMapper.toCustom(value)
+    case PbArray(xs)      => ArrayData(xs.map(paramValueMapper.toCustom).to[mutable.WrappedArray])
+    case PbMatrix(xss) =>
+      MatrixData(
+        xss
+          .map(xs => paramValueMapper.toCustom(xs).asInstanceOf[ArrayData[Any]].values.to[mutable.WrappedArray])
+          .to[mutable.WrappedArray]
+      )
+    case PbStruct(xs) => Struct(xs.map(x => paramMapper.toCustom(x)).toSet)
+  } {
+    case x: Int          => PbInt(x)
+    case x: Long         => PbLong(x)
+    case x: Float        => PbFloat(x)
+    case x: Short        => PbShort(x)
+    case x: Double       => PbDouble(x)
+    case x: Char         => PbChar(x)
+    case x: Byte         => PbBytes(ByteString.copyFrom(Array(x)))
+    case x: Boolean      => PbBoolean(x)
+    case x: String       => PbString(x)
+    case Choice(value)   => PbChoice(value)
+    case RaDec(ra, dec)  => PbRaDec(ra, dec)
+    case x: UTCTime      => utcTimeTypeMapper.toBase(x)
+    case TAITime(value)  => PbTAITime(Some(Timestamp(value.getEpochSecond, value.getNano)))
+    case ArrayData(xs)   => PbArray().withValues(xs.map(paramValueMapper.toBase))
+    case MatrixData(xss) => PbMatrix().withValues(xss.map(xs => paramValueMapper.toBase(ArrayData(xs)).asInstanceOf[PbArray]))
+    case Struct(xs)      => PbStruct().withParamSet(xs.map(x => paramMapper.toBase(x.asInstanceOf[Parameter[Any]])).toSeq)
+  }
+
+  lazy val paramMapper: TypeMapper[PbParameter, Parameter[_]] = TypeMapper[PbParameter, Parameter[_]](
+    pbParameter =>
+      Parameter(
+        pbParameter.name,
+        pbParameter.keyType.asInstanceOf[KeyType[Any]],
+        pbParameter.values.map(paramValueMapper.toCustom).to[mutable.WrappedArray],
+        pbParameter.units
+      )(pbParameter.keyType.sFormat.asInstanceOf[Format[Any]], pbParameter.keyType.sClassTag.asInstanceOf[ClassTag[Any]])
+  )(
+    parameter =>
+      PbParameter()
+        .withName(parameter.keyName)
+        .withKeyType(parameter.keyType)
+        .withUnits(parameter.units)
+        .withValues(
+          parameter.items.map(paramValueMapper.toBase)
+      )
+  )
+
+  private[csw] implicit def eventTypeMapper[T <: Event]: TypeMapper[PbEvent, T] = new TypeMapper[PbEvent, T] {
+    override def toCustom(base: PbEvent): T = {
+      val factory: (Id, Prefix, EventName, UTCTime, Set[Parameter[_]]) ⇒ Any = base.eventType match {
+        case PbEventType.SystemEvent     ⇒ SystemEvent.apply
+        case PbEventType.ObserveEvent    ⇒ ObserveEvent.apply
+        case PbEventType.Unrecognized(x) ⇒ throw new RuntimeException(s"unknown event type=[${base.eventType.toString} :$x]")
+      }
+
+      factory(
+        Id(base.eventId),
+        Prefix(base.source),
+        EventName(base.name),
+        base.eventTime.map(utcTimeTypeMapper.toCustom).get,
+        base.paramSet.map(paramMapper.toCustom).toSet
+      ).asInstanceOf[T]
+    }
+
+    override def toBase(custom: T): PbEvent = {
+      val pbEventType = custom match {
+        case _: ObserveEvent ⇒ PbEventType.ObserveEvent
+        case _: SystemEvent  ⇒ PbEventType.SystemEvent
+      }
+      PbEvent()
+        .withEventId(custom.eventId.id)
+        .withSource(custom.source.prefix)
+        .withName(custom.eventName.name)
+        .withEventTime(utcTimeTypeMapper.toBase(custom.eventTime))
+        .withParamSet(custom.paramSet.map(x => paramMapper.toBase(x)).toSeq)
+        .withEventType(pbEventType)
+    }
+  }
+
+  implicit val unitsTypeMapper: TypeMapper[PbUnits, Units] =
+    TypeMapper[PbUnits, Units](x ⇒ Units.withName(x.toString()))(x ⇒ PbUnits.fromName(x.toString).get)
+
+  implicit val keyTypeTypeMapper: TypeMapper[PbKeyType, KeyType[_]] =
+    TypeMapper[PbKeyType, KeyType[_]](x ⇒ KeyType.withName(x.toString()))(x ⇒ PbKeyType.fromName(x.toString).get)
+
+}
