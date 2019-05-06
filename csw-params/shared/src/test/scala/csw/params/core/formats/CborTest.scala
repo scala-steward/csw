4932993f5711b9dca089a9407fdfc565c8e7b9e2
Dollyg <dollyg@thoughtworks.com>
2019-05-06 20:58:43 +0530

    fix paramJs compilation by moving interop tests to jvm scope

--- csw-params/shared/src/test/scala/csw/params/core/formats/CborTest.scala
+++ csw-params/shared/src/test/scala/csw/params/core/formats/CborTest.scala
@@ -1,13 +1,11 @@
 package csw.params.core.formats
 
 import csw.params.core.formats.CborSupport._
-import csw.params.core.generics.{Key, Parameter}
 import csw.params.core.models._
 import csw.params.events.{EventName, ObserveEvent, SystemEvent}
 import csw.params.testdata.ParamSetData
 import io.bullet.borer.Cbor
 import org.scalatest.{FunSuite, Matchers}
-import csw.params.javadsl.JKeyType
 
 class CborTest extends FunSuite with Matchers {
 
@@ -30,23 +28,5 @@ class CborTest extends FunSuite with Matchers {
     val parsedEvent = Cbor.decode(bytes).to[SystemEvent].value
 
     parsedEvent shouldEqual event
-  }
-
-  test("byte key interop from java to scala") {
-    val jByteKey: Key[java.lang.Byte]    = JKeyType.ByteKey.make("bytes")
-    val param: Parameter[java.lang.Byte] = jByteKey.set("abc".getBytes().map(x ⇒ x: java.lang.Byte))
-    val bytes: Array[Byte]               = Cbor.encode(param).toByteArray
-
-    val parsedParam = Cbor.decode(bytes).to[Parameter[Byte]].value
-    parsedParam shouldEqual param
-  }
-
-  test("int key interop from java to scala") {
-    val jIntKey: Key[Integer]     = JKeyType.IntKey.make("ints")
-    val param: Parameter[Integer] = jIntKey.set(Array(1, 2, 3).map(x ⇒ Integer.valueOf(x)))
-    val bytes: Array[Byte]        = Cbor.encode(param).toByteArray
-
-    val parsedParam = Cbor.decode(bytes).to[Parameter[Int]].value
-    parsedParam shouldEqual param
   }
 }
