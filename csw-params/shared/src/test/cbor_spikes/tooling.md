8a8fb2e6f54d401ecba79590b8262a5b00ea23e7
Dollyg <dollyg@thoughtworks.com>
2019-05-06 12:43:01 +0530

    Add test for interop within java and scala keytpes

--- csw-params/shared/src/test/cbor_spikes/tooling.md
+++ csw-params/shared/src/test/cbor_spikes/tooling.md
@@ -7,13 +7,13 @@
 
 #### For validating cbor against the schema
 
-`cddl csw-event/csw-event-client/src/test/cbor_spikes/schema.cddl validate /tmp/input.cbor`
+`cddl csw-params/shared/src/test/cbor_spikes/schema.cddl validate /tmp/input.cbor`
 
 #### For generating sample jsons for your schema
 
 This command helps while writing schema. It guides you with what all data can fit into the schema.
 
-`cddl csw-event/csw-event-client/src/test/cbor_spikes/schema.cddl json-generate 5`
+`cddl csw-params/shared/src/test/cbor_spikes/schema.cddl json-generate 5`
 
 #### more tools 
 Explore: https://github.com/cabo/cbor-diag
--- csw-params/shared/src/test/scala/csw/params/core/formats/CborTest.scala
+++ csw-params/shared/src/test/scala/csw/params/core/formats/CborTest.scala
@@ -1,23 +1,52 @@
 package csw.params.core.formats
 
 import csw.params.core.formats.CborSupport._
+import csw.params.core.generics.{Key, Parameter}
 import csw.params.core.models._
-import csw.params.events.{EventName, ObserveEvent}
+import csw.params.events.{EventName, ObserveEvent, SystemEvent}
 import csw.params.testdata.ParamSetData
 import io.bullet.borer.Cbor
 import org.scalatest.{FunSuite, Matchers}
+import csw.params.javadsl.JKeyType
 
 class CborTest extends FunSuite with Matchers {
 
   private val prefix: Prefix       = Prefix("wfos.blue.filter")
   private val eventName: EventName = EventName("filter wheel")
 
-  test("should able to serialize and deserialize event with paramSet having all key-types") {
+  test("should able to serialize and deserialize observe event with paramSet having all key-types") {
     val event = ObserveEvent(prefix, eventName, ParamSetData.paramSet)
 
     val bytes       = Cbor.encode(event).toByteArray
     val parsedEvent = Cbor.decode(bytes).to[ObserveEvent].value
 
     parsedEvent shouldEqual event
+  }
+
+  test("should able to serialize and deserialize system event with paramSet having all key-types") {
+    val event = SystemEvent(prefix, eventName, ParamSetData.paramSet)
+
+    val bytes       = Cbor.encode(event).toByteArray
+    val parsedEvent = Cbor.decode(bytes).to[SystemEvent].value
+
+    parsedEvent shouldEqual event
+  }
+
+  test("byte key interop from java to scala") {
+    val jByteKey: Key[java.lang.Byte]    = JKeyType.ByteKey.make("bytes")
+    val param: Parameter[java.lang.Byte] = jByteKey.set("abc".getBytes().map(x ⇒ x: java.lang.Byte))
+    val bytes: Array[Byte]               = Cbor.encode(param).toByteArray
+
+    val parsedParam = Cbor.decode(bytes).to[Parameter[Byte]].value
+    parsedParam shouldEqual param
+  }
+
+  test("int key interop from java to scala") {
+    val jIntKey: Key[Integer]     = JKeyType.IntKey.make("ints")
+    val param: Parameter[Integer] = jIntKey.set(Array(1, 2, 3).map(x ⇒ Integer.valueOf(x)))
+    val bytes: Array[Byte]        = Cbor.encode(param).toByteArray
+
+    val parsedParam = Cbor.decode(bytes).to[Parameter[Int]].value
+    parsedParam shouldEqual param
   }
 }
