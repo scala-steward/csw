3c57c47e3e4b9d002c763fa37c7eca0a534e2597
Dollyg <dollyg@thoughtworks.com>
2019-05-10 15:33:19 +0530

    use unicode strings in python

--- csw-params/shared/src/test/cbor_spikes/sample.py
+++ csw-params/shared/src/test/cbor_spikes/sample.py
@@ -4,21 +4,36 @@ import pprint
 
 
 def toParameterSet(paramSet):
-    return map(lambda param: Parameter(param), paramSet)
+    return map(lambda param: Parameter.from_dict(param), paramSet)
 
 
 class Parameter:
-    def __init__(self, obj):
+    def __init__(self, key_name, key_type, items, units):
+        self.key_name = key_name
+        self.key_type = key_type
+        self.items = items
+        self.units = units
 
-        self.key_name = obj['keyName']
-        self.key_type = obj['keyType']
-
+    @classmethod
+    def from_dict(self, obj):
+        key_name = obj['keyName']
+        key_type = obj['keyType']
         if obj['keyType'] == 'StructKey':
-            self.items = map(lambda item: toParameterSet(item['paramSet']), obj['items'])
+            items = map(lambda item: toParameterSet(item['paramSet']), obj['items'])
         else:
-            self.items = obj['items']
+            items = obj['items']
 
-        self.units = obj['units']
+        units = obj['units']
+        return Parameter(key_name, key_type, items, units)
+
+    def to_dict(self):
+        items = self.items if not self.key_type == "StructKey" else map(lambda item: item.to_dict, self.items)
+        return {
+            u"keyName": self.key_name,
+            u"keyType": self.key_type,
+            u"items": items,
+            u"units": self.units
+        }
 
     def __repr__(self):
         return "Parameter(keyName=" + self.key_name + \
@@ -28,12 +43,40 @@ class Parameter:
 
 
 class Event:
-    def __init__(self, obj):
-        self.id = obj['eventId']
-        self.prefix = obj['source']
-        self.event_name = obj['eventName']
-        self.event_time = obj['eventTime']
-        self.param_set = toParameterSet(obj['paramSet'])
+    param_set = None
+    id = None
+    prefix = None
+    event_name = None
+    event_time = None
+
+    @classmethod
+    def from_dict(self, obj):
+        id = obj['eventId']
+        prefix = obj['source']
+        event_name = obj['eventName']
+        event_time = obj['eventTime']
+        param_set = toParameterSet(obj['paramSet'])
+        return Event(id, prefix, event_name, event_time, param_set)
+
+    def to_dict(self):
+        return [u"ObserveEvent", {
+            u"eventId": self.id,
+            u"source": self.prefix,
+            u"eventName": self.event_name,
+            u"eventTime": self.event_time,
+            u"paramSet": map(lambda param: param.to_dict(), self.param_set)
+        }]
+
+    def __init__(self, id, prefix, event_name, event_time, param_set):
+        self.id = id
+        self.prefix = prefix
+        self.event_name = event_name
+        self.event_time = event_time
+        self.param_set = param_set
+
+    @classmethod
+    def default_encoder(self, encoder, value):
+        encoder.encode(self.to_dict(value))
 
     def __repr__(self):
         return "id=" + self.id + "\n" + \
@@ -46,10 +89,12 @@ class Event:
 my_path = os.path.abspath(os.path.dirname(__file__))
 input_cbor_path = os.path.join(my_path, "sample_encoded_data/event.cbor")
 
-with open(input_cbor_path, 'rb') as fp:
-    obj = load(fp)
-    # pp = pprint.PrettyPrinter(indent=1, depth=5)
-    # pp.pprint(obj)
+e = Event(u"id", u"prefix", u"eventname", {u"nanos": 403, u"seconds": 15},
+          set([Parameter(u"ints", u"IntKey", [0], u"NoUnits")]))
 
-    event = Event(obj)
-    print "====== Decoded Event ======\n" + str(event)
+with open(input_cbor_path, 'wb') as fp:
+    dump(e, fp, default=Event.default_encoder)
+
+with open(input_cbor_path, 'rb') as fp:
+    [type, obj] = load(fp)
+    print Event.from_dict(obj)
